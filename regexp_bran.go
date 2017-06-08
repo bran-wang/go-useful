1e99463cbfd0a8fa6b6edc719d59aa1a7a51e1ea
bran-wang <branw@vmware.com>
2017-06-08 15:45:44 +0800

    regexp moudle

--- regexp_bran.go
+++ regexp_bran.go
@@ -8,7 +8,7 @@ import "regexp"
 func main() {
 
     var name = "bran"
-    res,_ := regexp.Match("qemu()", []byte("qemu()001"))
+    res,_ := regexp.Match("aemu()|01", []byte("qemu()001"))
     fmt.Println(res)
     if ok,_ := regexp.Match("qemum", []byte("qemu()001")); ok || name=="bran" {
         fmt.Println(ok)
